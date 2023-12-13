95f6b1b97b4ed0a2bb32a6a406568629414a4b3f
Danishaustin <142653590+Danishaustin@users.noreply.github.com>
2023-12-13 08:31:58 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon',
+        method: 'GET',
+        success: function (data) {
+            console.log("Data received from API:", data);
+            displayPokemon(data);
+        },
+        error: function (error) {
+            console.error("Error fetching data: ", error);
+        }
+    });
+
+    function displayPokemon(data) {
+        if (data && data.results) {
+            let output = '';
+    
+            $.each(data.results, function (index, item) {
+                output += `
+                <div class="col-sm-6 col-lg-3 d-flex">
+                    <div class="card text-center shadow bg-white border border-secondary rounded mb-3" style="width: 30rem;">
+                        <div class="card-body">
+                            <p class="card-text">${item.name}</p>
+                            <a href="${item.url}" class="btn btn-primary">Detail</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+            });
+    
+            $('#pokemonList').html(output);
+        } else {
+            console.error("Invalid data format: ", data);
+        }
+    }
+});
